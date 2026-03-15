3efb96a8974b87400497c0c96fb6212766c62dd9
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-03-15 12:58:38 +0530

    Create

---
+++ problem-1773559718247/version/java/problem-1773559718247_20260315_125838.java
@@ -0,0 +1,15 @@
+// Last updated: 3/15/2026, 12:58:38 PM
+1class Solution {
+2    public int countCommas(int n) {
+3       int y=(n-1000)+1;
+4        if (n<999)
+5            return 0;
+6        if(n>999){
+7            
+8         
+9            return y; 
+10        }
+11        return y;
+12    }
+13   
+14}
