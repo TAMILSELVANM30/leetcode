cf19798683985749e6239c3254523119d7602239
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-06-11 01:30:03 +0530

    Create 4245-CountCommasInRange

---
+++ 4245-CountCommasInRange/4245-CountCommasInRange.java
@@ -0,0 +1,15 @@
+// Last updated: 6/11/2026, 1:30:03 AM
+class Solution {
+    public int countCommas(int n) {
+       int y=(n-1000)+1;
+        if (n<999)
+            return 0;
+        if(n>999){
+            
+         
+            return y; 
+        }
+        return y;
+    }
+   
+}
