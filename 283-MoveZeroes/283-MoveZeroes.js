4ff0e5680f40e7916e9332498589378004103023
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-07-07 22:25:26 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.js
@@ -0,0 +1,13 @@
+// Last updated: 7/7/2026, 10:25:26 PM
+var moveZeroes = function(nums) {
+    let left = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] !== 0) {
+            [nums[right], nums[left]] = [nums[left], nums[right]];
+            left++;
+        }
+    }
+
+    return nums;    
+};
