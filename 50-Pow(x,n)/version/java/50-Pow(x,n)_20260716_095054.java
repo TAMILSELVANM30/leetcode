152f322e72d975f6db6cbe14124d92935887e01b
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-07-16 09:50:52 +0530

    Create 50-Pow(x,n)

---
+++ 50-Pow(x,n)/version/java/50-Pow(x,n)_20260716_095054.java
@@ -0,0 +1,22 @@
+// Last updated: 7/16/2026, 9:50:54 AM
+1class Solution {
+2    public double myPow(double x, int n) {
+3        long N=n;
+4        if(n<0){
+5            x=1/x;
+6            N=-N;
+7        }
+8        return power(x,N);
+9    }
+10    public double power(double x, long N){
+11        if(N==0){
+12            return 1;
+13        }
+14        double half=power(x,N/2);
+15        if(N%2==0){
+16            return half*half;
+17        }else{
+18            return half*half*x;
+19        }
+20    }
+21}
