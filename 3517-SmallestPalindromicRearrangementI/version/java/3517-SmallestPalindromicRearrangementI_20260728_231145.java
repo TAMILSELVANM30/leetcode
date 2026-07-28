c159ac14e95db4fd1a829ef8f059568a509b8b27
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-07-28 23:11:45 +0530

    Create 3517-SmallestPalindromicRearrangementI

---
+++ 3517-SmallestPalindromicRearrangementI/version/java/3517-SmallestPalindromicRearrangementI_20260728_231145.java
@@ -0,0 +1,19 @@
+// Last updated: 7/28/2026, 11:11:45 PM
+class Solution {
+    public String smallestPalindrome(String s) {
+        int[] c = new int[26];
+        int n = s.length();
+        String middle = (n % 2 == 0) ? "" : String.valueOf(s.charAt(n >> 1));
+        StringBuilder ans = new StringBuilder();
+        n >>= 1;
+        for(int i = 0; i < n; i++){
+            c[s.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < 26; i++){
+            ans.append(String.valueOf((char)(i + 'a')).repeat(c[i]));
+        }
+
+        return ans.toString() + middle + ans.reverse().toString();
+    }
+}
