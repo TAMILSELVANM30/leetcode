b2fd4df49b7efaba44bea592ff4dfbe631cfbfdc
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-08-12 23:11:51 +0530

    Create 3658-GCDofOddandEvenSums

---
+++ 3658-GCDofOddandEvenSums/version/java/3658-GCDofOddandEvenSums_20260812_231152.java
@@ -0,0 +1,15 @@
+// Last updated: 8/12/2026, 11:11:52 PM
+1class Solution {
+2    public int gcdOfOddEvenSums(int n) {
+3        int sumodd=n*n;
+4        int sumeven=n*n+n;
+5        return GCD(sumodd,sumeven);
+6        
+7    }
+8    public int GCD(int sumodd,int sumeven){
+9        if(sumeven==0){
+10            return sumodd;
+11        }
+12        return GCD(sumeven, sumodd % sumeven);
+13    }
+14}
