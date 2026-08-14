3b5d5f05831db322b4db4516bd7ac4eab7fecd24
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-08-14 21:16:30 +0530

    Create 1344-AngleBetweenHandsofaClock

---
+++ 1344-AngleBetweenHandsofaClock/version/python/1344-AngleBetweenHandsofaClock_20260814_211630.py
@@ -0,0 +1,12 @@
+# Last updated: 8/14/2026, 9:16:30 PM
+1class Solution(object):
+2    def angleClock(self, hour, minutes):
+3        """
+4        :type hour: int
+5        :type minutes: int
+6        :rtype: float
+7        """
+8        hour_angle = (hour % 12) * 30 + minutes * 0.5
+9        minute_angle = minutes * 6
+10        diff = abs(hour_angle - minute_angle)
+11        return min(diff, 360 - diff)
