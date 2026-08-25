ea7d45db2cdc955783bf2e1e180d637458363cb9
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-08-25 20:04:32 +0530

    Create 3718-SmallestMissingMultipleofK

---
+++ 3718-SmallestMissingMultipleofK/version/java/3718-SmallestMissingMultipleofK_20260825_200433.java
@@ -0,0 +1,21 @@
+// Last updated: 8/25/2026, 8:04:33 PM
+1class Solution {
+2    public int missingMultiple(int[] nums, int k) {
+3        HashSet s= new HashSet<>();
+4        int n=nums.length;
+5        for(int i:nums){
+6            s.add(i);
+7        }
+8       
+9        int mini=1;
+10      
+11        while((mini%k!=0)|| (s.contains(mini))){
+12                
+13                mini++;
+14
+15        }
+16        
+17        
+18        return mini;
+19    }
+20}
