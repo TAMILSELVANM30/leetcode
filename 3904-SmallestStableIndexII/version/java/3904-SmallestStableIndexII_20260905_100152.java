9ffc05143b6ed0fe3b88d5475dbe8f4501be8cc9
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-09-05 10:01:53 +0530

    Create 3904-SmallestStableIndexII

---
+++ 3904-SmallestStableIndexII/version/java/3904-SmallestStableIndexII_20260905_100152.java
@@ -0,0 +1,30 @@
+// Last updated: 9/5/2026, 10:01:52 AM
+1class Solution {
+2    public int firstStableIndex(int[] nums, int k) {
+3        int n=nums.length;
+4        int i=n-1;
+5        int[] sufixsum = new  int[n];
+6        int[] prefixsum=new int[n];
+7       int  mini=Integer.MAX_VALUE;
+8        while(0<=i){
+9           
+10            mini=Math.min(mini,nums[i]);
+11            sufixsum[i]=mini;
+12            i--;
+13        }
+14        i=0;
+15        int maxi=0;
+16       while(i<n){
+17          maxi=Math.max(maxi,nums[i]);
+18         prefixsum[i]=maxi;   
+19        i++;
+20       }
+21       for(int j=0;j<n;j++){
+22        if(prefixsum[j]-sufixsum[j]<=k){
+23            return j;
+24        }
+25       }
+26       return -1;
+27        
+28    }
+29}
