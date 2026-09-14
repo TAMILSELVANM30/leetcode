437dd752e223a77f1face6fda2c4be079abb86e7
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-09-14 23:56:08 +0530

    Create 1018-BinaryPrefixDivisibleBy5

---
+++ 1018-BinaryPrefixDivisibleBy5/1018-BinaryPrefixDivisibleBy5.cpp
@@ -0,0 +1,13 @@
+// Last updated: 9/14/2026, 11:56:08 PM
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> ans;
+        int val = 0;
+        for (int bit : nums) {
+            val = (val * 2 + bit) % 5;
+            ans.push_back(val == 0);
+        }
+        return ans;
+    }
+};
