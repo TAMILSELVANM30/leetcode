673f23a7ef19f310dd2bb5cd2f02b74411919fbf
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-09-14 23:55:39 +0530

    Create 1732-FindTheHighestAltitude

---
+++ 1732-FindTheHighestAltitude/1732-FindTheHighestAltitude.java
@@ -0,0 +1,12 @@
+// Last updated: 9/14/2026, 11:55:39 PM
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int alt=0;
+        int m_alt=0;
+        for (int s:gain){
+            alt+=s;
+         m_alt=Math.max(alt,m_alt);
+        }
+        return m_alt;
+    }
+}
